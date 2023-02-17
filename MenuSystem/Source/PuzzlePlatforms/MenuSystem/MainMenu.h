236c1258070e6c484d9b7202a8b288561579dd5f
Dave Schaub <schaub818@gmail.com>
2023-02-16 19:01:08 -0500

    Connecting UMG to C++ end

---
+++ MenuSystem/Source/PuzzlePlatforms/MenuSystem/MainMenu.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MainMenu.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class PUZZLEPLATFORMS_API UMainMenu : public UUserWidget
+{
+	GENERATED_BODY()
+
+private:
+	UPROPERTY(meta = (BindWidget))
+	class UButton* BTN_Host;
+
+	UPROPERTY(meta = (BindWidget))
+	class UButton* BTN_Join;
+};
